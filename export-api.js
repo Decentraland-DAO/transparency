8df871dd4500553acfba959a759f3b1ed027a7d8
Yemel Jardi <angel.jardi@gmail.com>
2022-01-28 11:46:08 -0300

    update on apis

--- export-api.js
+++ export-api.js
@@ -5,13 +5,34 @@ const balances = require('./public/balances.json');
 async function main() {
     const data = {
         'balances': balances,
-        'income': [],
-        'expenses': [],
-        'funding': [],
+        'income': {
+            'total': 1200,
+            'previous': 2.3,
+            'details': [
+                {'name': 'Vesting Contract', 'value': 1233333},
+                {'name': 'Marketplace', 'value': 1233333},
+                {'name': 'Wearables Publication', 'value': 1233333},
+                {'name': 'Other', 'value': 2000},
+            ]
+        },
+        'expenses': {
+            'total': 1200,
+            'previous': 2.3,
+            'details': [
+                {'name': 'Vesting Contract', 'value': 1233333},
+                {'name': 'Curation Committee', 'value': 222233},
+                {'name': 'DAO Facilitator', 'value': 2000},
+                {'name': 'Other', 'value': 2000},
+            ]
+        },
+        'funding': {
+            'total': 28000,
+            'budget': 40000,
+        },
         'members': [
-            {'address': '', 'name': 'Yemel', 'team': 'DAO Committee'},
-            {'address': '', 'name': 'Yemel', 'team': 'DAO Committee'},
-            {'address': '', 'name': 'Yemel', 'team': 'DAO Committee'},
+            {'address': '0xfe91C0c482E09600f2d1DBCA10FD705BC6de60bc', 'name': 'Yemel', 'team': 'DAO Committee'},
+            {'address': '0xfe91C0c482E09600f2d1DBCA10FD705BC6de60bc', 'name': 'HPrivakos', 'team': 'DAO Committee'},
+            {'address': '0xfe91C0c482E09600f2d1DBCA10FD705BC6de60bc', 'name': 'Eric', 'team': 'DAO Committee'},
         ],
     };
 
