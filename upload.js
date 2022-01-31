a7558d5ea8c277d202aadecdae74554808f8ae27
Yemel Jardi <angel.jardi@gmail.com>
2022-01-31 15:33:58 -0300

    new workflow

--- upload.js
+++ upload.js
@@ -5,16 +5,11 @@ const { parse } = require('csv-parse/sync');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 async function main() {
-    console.log('Client id', process.env.SHEET_ID.length);
-    console.log('Client email', process.env.SHEET_CLIENT_EMAIL.length);
-    console.log('Client secret', process.env.SHEET_PRIVATE_KEY.slice(0,500));
-
     const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
       private_key: process.env.SHEET_PRIVATE_KEY,
     });
-
     
     const title = process.argv[2];
     const path = process.argv[3];
