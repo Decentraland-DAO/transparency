f612c348f9e0e65e4567f3c3dc5b9d82638fdeef
Nicolás Comerci <ncomerci@itba.edu.ar>
2022-04-26 22:03:04 -0300

    export-members typed

--- src/export-members.ts
+++ src/export-members.ts
@@ -1,8 +1,9 @@
 import snapshot from '@snapshot-labs/snapshot.js'
+import { STRATEGIES, Vote } from './interfaces/Members'
 import { fetchGraphQL, saveToCSV, saveToJSON } from './utils'
 require('dotenv').config()
 
-type MemberInfo = {
+interface MemberInfo {
   address: string
   totalVP: number
   manaVP: number
@@ -11,176 +12,31 @@ type MemberInfo = {
   delegatedVP: number
 }
 
-type Vote = {
-  voter: string
-}
-
-const info: MemberInfo[] = []
-
 const space = 'snapshot.dcl.eth'
-const strategies = [
-  {
-    "name": "erc20-balance-of",
-    "params": {
-      "symbol": "WMANA",
-      "address": "0xfd09cf7cfffa9932e33668311c4777cb9db3c9be",
-      "decimals": 18
-    }
-  },
-  {
-    "name": "multichain",
-    "params": {
-      "name": "multichain",
-      "graphs": {
-        "137": "https://api.thegraph.com/subgraphs/name/decentraland/blocks-matic-mainnet"
-      },
-      "symbol": "MANA",
-      "strategies": [
-        {
-          "name": "erc20-balance-of",
-          "params": {
-            "address": "0x0f5d2fb29fb7d3cfee444a200298f468908cc942",
-            "decimals": 18
-          },
-          "network": "1"
-        },
-        {
-          "name": "erc20-balance-of",
-          "params": {
-            "address": "0xA1c57f48F0Deb89f569dFbE6E2B7f46D33606fD4",
-            "decimals": 18
-          },
-          "network": "137"
-        }
-      ]
-    }
-  },
-  {
-    "name": "erc721-with-multiplier",
-    "params": {
-      "symbol": "LAND",
-      "address": "0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d",
-      "multiplier": 2000
-    }
-  },
-  {
-    "name": "decentraland-estate-size",
-    "params": {
-      "symbol": "ESTATE",
-      "address": "0x959e104e1a4db6317fa58f8295f586e1a978c297",
-      "multiplier": 2000
-    }
-  },
-  {
-    "name": "erc721-with-multiplier",
-    "params": {
-      "symbol": "NAMES",
-      "address": "0x2a187453064356c898cae034eaed119e1663acb8",
-      "multiplier": 100
-    }
-  },
-  {
-    "name": "delegation",
-    "params": {
-      "symbol": "VP (delegated)",
-      "strategies": [
-        {
-          "name": "erc20-balance-of",
-          "params": {
-            "symbol": "WMANA",
-            "address": "0xfd09cf7cfffa9932e33668311c4777cb9db3c9be",
-            "decimals": 18
-          }
-        },
-        {
-          "name": "erc721-with-multiplier",
-          "params": {
-            "symbol": "LAND",
-            "address": "0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d",
-            "multiplier": 2000
-          }
-        },
-        {
-          "name": "decentraland-estate-size",
-          "params": {
-            "symbol": "ESTATE",
-            "address": "0x959e104e1a4db6317fa58f8295f586e1a978c297",
-            "multiplier": 2000
-          }
-        },
-        {
-          "name": "multichain",
-          "params": {
-            "name": "multichain",
-            "graphs": {
-              "137": "https://api.thegraph.com/subgraphs/name/decentraland/blocks-matic-mainnet"
-            },
-            "symbol": "MANA",
-            "strategies": [
-              {
-                "name": "erc20-balance-of",
-                "params": {
-                  "address": "0x0f5d2fb29fb7d3cfee444a200298f468908cc942",
-                  "decimals": 18
-                },
-                "network": "1"
-              },
-              {
-                "name": "erc20-balance-of",
-                "params": {
-                  "address": "0xA1c57f48F0Deb89f569dFbE6E2B7f46D33606fD4",
-                  "decimals": 18
-                },
-                "network": "137"
-              }
-            ]
-          }
-        },
-        {
-          "name": "erc721-with-multiplier",
-          "params": {
-            "symbol": "NAMES",
-            "address": "0x2a187453064356c898cae034eaed119e1663acb8",
-            "multiplier": 100
-          }
-        }
-      ]
-    }
-  }
-]
 
 const network = '1'
 const blockNumber = 'latest'
-
-const catalysts = [
-  'peer-ec1.decentraland.org',
-  'peer-wc1.decentraland.org',
-  'eer-eu1.decentraland.org',
-  'peer-ap1.decentraland.org',
-  'interconnected.online',
-  'peer.decentral.io',
-  'peer.melonwave.com',
-  'peer.kyllian.me',
-  'peer.uadevops.com',
-  'peer.dclnodes.io',
-]
 
 async function main() {
   // Fetch Snapshot Votes
   const url = 'https://hub.snapshot.org/graphql'
   const where = 'space_in: ["snapshot.dcl.eth"], vp_gt: 10'
-  let votes: Vote[] = await fetchGraphQL(url, 'votes', where, 'created', 'voter')
+  const votes: Vote[] = await fetchGraphQL(url, 'votes', where, 'created', 'voter')
 
   const allVoters = votes.map(v => v.voter)
   const members = allVoters.filter((elem, pos) => allVoters.indexOf(elem) == pos)
   console.log("Total Members:", members.length)
 
-  const getMemberInfo = async (address: string) => {
-    let scores = [0, 0, 0, 0, 0]
+  const info: MemberInfo[] = []
+
+  for (const address of members) {
+    let scores = [0, 0, 0, 0, 0, 0]
     try {
-      scores = await snapshot.utils.getScores(space, strategies, network, [address], blockNumber)
-      scores = scores.map(score => parseInt(score[address] || 0))
-    } catch { }
+      const snapshotScores = await snapshot.utils.getScores(space, STRATEGIES, network, [address], blockNumber)
+      scores = snapshotScores.map(score => parseInt(score[address] || 0))
+    } catch (e) {
+      console.error(address)
+    }
 
     info.push({
       address,
@@ -190,15 +46,8 @@ async function main() {
       namesVP: scores[4],
       delegatedVP: scores[5],
     })
+    console.log(info.length, members.length, info.length / members.length * 100);
   }
-
-  const membersInfo: Promise<void>[] = []
-
-  for (let i = 0; i < members.length; i++) {
-    membersInfo.push(getMemberInfo(members[i]))
-  }
-
-  await Promise.all(membersInfo)
 
   saveToJSON('members.json', info)
   saveToCSV('members.csv', info, [
