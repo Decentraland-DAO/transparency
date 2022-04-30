b6f0ca24c25c29cb6eb787b3c3d30890e980e0f6
Nicolás Comerci <ncomerci@itba.edu.ar>
2022-04-30 15:54:15 -0300

    export-wearables typed

--- src/export-wearables.ts
+++ src/export-wearables.ts
@@ -1,28 +1,32 @@
-import BigNumber from "bignumber.js";
-import { fetchGraphQL, saveToCSV, saveToJSON } from "./utils";
+import BigNumber from "bignumber.js"
+import { Wearable, WearableData } from "./interfaces/Wearable"
+import { fetchGraphQLCondition, saveToCSV, saveToJSON } from "./utils"
+
+type WearableParsed = Wearable & WearableData
 
 async function main() {
   // Fetch Wearables
-  const url = 'https://api.thegraph.com/subgraphs/name/decentraland/collections-matic-mainnet';
-  let wearables = await fetchGraphQL(url, 'items', '', 'createdAt',
-    'id creator itemType totalSupply maxSupply rarity creationFee available price beneficiary URI image createdAt updatedAt reviewedAt soldAt sales volume metadata { wearable { name description category } }', 1000
-  );
+  const url = 'https://api.thegraph.com/subgraphs/name/decentraland/collections-matic-mainnet'
+  const wearables: WearableParsed[] = await fetchGraphQLCondition(url, 'items', 'createdAt', 'id',
+    'id creator itemType totalSupply maxSupply rarity creationFee available price beneficiary URI image createdAt updatedAt reviewedAt soldAt sales volume metadata { wearable { name description category } }'
+  )
 
   for (const w of wearables) {
-    w.name = w.metadata.wearable.name;
-    w.description = w.metadata.wearable.description;
-    w.category = w.metadata.wearable.category;
-    w.price = new BigNumber(w.price).dividedBy(10 ** 18).toNumber() || 0;
-    w.creationFee = new BigNumber(w.creationFee).dividedBy(10 ** 18).toNumber() || 0;
+    w.name = w.metadata.wearable?.name
+    w.description = w.metadata.wearable?.description
+    w.category = w.metadata.wearable?.category
+    w.price = new BigNumber(w.price).dividedBy(10 ** 18).toNumber() || 0
+    w.creationFee = new BigNumber(w.creationFee).dividedBy(10 ** 18).toNumber() || 0
 
-    w.createdAt = w.createdAt && new Date(w.createdAt * 1000).toISOString();
-    w.updatedAt = w.updatedAt && new Date(w.updatedAt * 1000).toISOString();
-    w.reviewedAt = w.reviewedAt && new Date(w.reviewedAt * 1000).toISOString();
-    w.soldAt = w.soldAt && new Date(w.soldAt * 1000).toISOString();
+    w.createdAt = w.createdAt && new Date(parseInt(w.createdAt) * 1000).toISOString()
+    w.updatedAt = w.updatedAt && new Date(parseInt(w.updatedAt) * 1000).toISOString()
+    w.reviewedAt = w.reviewedAt && new Date(parseInt(w.reviewedAt) * 1000).toISOString()
+    w.soldAt = w.soldAt && new Date(parseInt(w.soldAt) * 1000).toISOString()
+
   }
 
-  console.log(wearables.length, 'wearables found.');
-  saveToJSON('wearables.json', wearables);
+  console.log(wearables.length, 'wearables found.')
+  saveToJSON('wearables.json', wearables)
   saveToCSV('wearables.csv', wearables, [
     { id: 'id', title: 'Item ID' },
     { id: 'name', title: 'Name' },
@@ -45,7 +49,7 @@ async function main() {
     { id: 'beneficiary', title: 'Beneficiary' },
     { id: 'URI', title: 'URI' },
     { id: 'urn', title: 'URN' },
-  ]);
+  ])
 }
 
-main();
+main()
---
+++ src/interfaces/Wearable.ts
@@ -0,0 +1,66 @@
+export interface Wearable {
+  id: string
+  creator: string
+  itemType: ItemType
+  totalSupply: number
+  maxSupply: number
+  rarity: Rarity
+  creationFee: number
+  available: number
+  price: number
+  beneficiary: string
+  URI: string
+  image: string
+  createdAt: string
+  updatedAt: string
+  reviewedAt: string
+  soldAt?: string
+  sales: number
+  volume: number
+  metadata: Metadata
+}
+
+export enum ItemType {
+  EmoteV1 = "emote_v1",
+  SmartWearableV1 = "smart_wearable_v1",
+  WearableV2 = "wearable_v2",
+}
+
+export interface Metadata {
+  wearable?: WearableData
+}
+
+export interface WearableData {
+  name: string
+  description: string
+  category: Category
+}
+
+export enum Category {
+  Earring = "earring",
+  Eyebrows = "eyebrows",
+  Eyes = "eyes",
+  Eyewear = "eyewear",
+  FacialHair = "facial_hair",
+  Feet = "feet",
+  Hair = "hair",
+  Hat = "hat",
+  Helmet = "helmet",
+  LowerBody = "lower_body",
+  Mask = "mask",
+  Mouth = "mouth",
+  Skin = "skin",
+  Tiara = "tiara",
+  TopHead = "top_head",
+  UpperBody = "upper_body",
+}
+
+export enum Rarity {
+  Common = "common",
+  Epic = "epic",
+  Legendary = "legendary",
+  Mythic = "mythic",
+  Rare = "rare",
+  Uncommon = "uncommon",
+  Unique = "unique",
+}
