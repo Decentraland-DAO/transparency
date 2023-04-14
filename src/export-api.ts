4b4c9462a0692a266b27049cd272d1f2a6790c58
ncomerci <ncomerci@itba.edu.ar>
2023-04-14 17:31:10 -0300

    fix: removed teams from api

--- src/export-api.ts
+++ src/export-api.ts
@@ -87,11 +87,6 @@ async function main() {
     'funding': {
       'total': totalFunding
     },
-    'teams': [
-      SABCommittee.toJson(),
-      DAOCommittee.toJson(),
-      CurationCommittee.toJson()
-    ],
     'committees': [
       SABCommittee.toJson(),
       DAOCommittee.toJson(),
