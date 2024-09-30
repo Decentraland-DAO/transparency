1d83b8e132b03da67a93c29cfea078347e0d8e53
lemu <github@lemu.sh>
2024-09-30 14:04:42 -0300

    chore: use alchemy and coingecko to fetch dao contracts balances (#97)

--- src/entities/Wallets.ts
+++ src/entities/Wallets.ts
@@ -11,7 +11,7 @@ enum WalletNames {
   DAO = "DAO Committee",
 }
 
-const WALLETS: Wallet[] = [
+export const WALLETS: Wallet[] = [
   { name: WalletNames.ARAGON, address: "0x9a6ebe7e2a7722f8200d0ffb63a1f6406a0d7dce", network: Networks.getEth() },
   { name: WalletNames.DAO, address: "0x89214c8ca9a49e60a3bfa8e00544f384c93719b1", network: Networks.getEth() },
   { name: WalletNames.DAO, address: "0xb08e3e7cc815213304d884c88ca476ebc50eaab2", network: Networks.getPolygon() },
